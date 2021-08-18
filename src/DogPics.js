92624944e668634ebf85ece137328827ef9f260a
Catherine Loesch <catherine.loesch@protonmail.com>
2021-08-18 13:32:03 +0100

    code-along completed

--- src/DogPics.js
+++ src/DogPics.js
@@ -1,22 +1,18 @@
 import React, { useState, useEffect } from "react";
 
-// WARNING: this useEffect will run in an infinite loop!
-// to fix, pass an empty array as the second argument for useEffect
 function DogPics() {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    console.log("useEffect");
     fetch("https://dog.ceo/api/breeds/image/random/3")
       .then((r) => r.json())
       .then((data) => {
-        console.log("setState");
-        setImages(data.message);
+        setImages(data.messages);
       });
-  });
+  }, []); 
 
-  console.log("render");
 
+  
   return (
     <div>
       {images.map((image) => (
@@ -24,6 +20,6 @@ function DogPics() {
       ))}
     </div>
   );
-}
+} 
 
-export default DogPics;
+export default DogPics
